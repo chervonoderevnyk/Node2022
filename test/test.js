7c39705f8128e6061eecc9d60c4d0963a1eef2e5
bigbird <igorchornyj@gmail.com>
2023-03-08 13:37:13 +0200

    lesson3

--- test/test.js
+++ test/test.js
@@ -1,13 +1,13 @@
 function logToConsole() {
-    console.log('FROM test/test.js');
+  console.log("FROM test/test.js");
 
-    console.log(__dirname);
+  console.log(__dirname);
 
-    console.log(__filename);
+  console.log(__filename);
 
-    console.log(process.cwd());
+  console.log(process.cwd());
 }
 
 module.exports = {
-    logToConsole,
-}
+  logToConsole,
+};
